b019e4704d9a8b651e2de49fbb4f3d6295b5e4f9
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-09-04 07:23:11 +0600

    category number

--- js/news.js
+++ js/news.js
@@ -13,15 +13,15 @@ const displayNews = (allNews) => {
     const newsDiv = document.createElement("div");
     newsDiv.classList.add("news");
     newsDiv.innerHTML = `
-    <div id="card" class="card my-3"">
-    <div class="row g-0">
-    <div class="col-md-4">
+    <div id="card" class="card mb-5 border-0">
+    <div class="row g-4 border-1 shadow-sm mb-3 bg-body rounded-3">
+    <div class="col-md-4 ">
       <img class="h-100" src="${
         news.thumbnail_url
       }" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8">
-      <div class="card-body">
+      <div class="card-body ">
         <h5 class="card-title">${news.title}</h5>
         <p class="card-text">${news.details.slice(0, 300)}</p>
         <div class="mx-4 ps-3 mb-4 d-lg-flex flex-lg-row">
@@ -46,8 +46,8 @@ const displayNews = (allNews) => {
 </div>
     `;
     newsContainer.appendChild(newsDiv);
-    // const totalNews = document.getElementById("total-news");
-    // totalNews.innerText = news.length;
+    const totalNews = document.getElementById("total-news");
+    totalNews.innerText = allNews.length;
   }
 };
 // loadNews();
